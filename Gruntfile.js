68b1c982e0e84e3a18c34ccfef19bf1d13e224f4
krishnaindia <krishnaof1988@gmail.com>
2014-09-09 17:26:41 +0530

    App structure modified and jscs integrated

---
+++ Gruntfile.js
@@ -0,0 +1,55 @@
+module.exports = function(grunt){
+
+  var path = require('path');
+ // var builder = require('./build/badge_generator')
+   
+
+  grunt.initConfig({
+      shell :{
+        test : {
+          command:path.join("node_modules",".bin","istanbul") +" cover " + path.join("node_modules", "mocha", "bin", "_mocha") +" -- -R mocha-unfunk-reporter",
+          options: {
+              stdout: true
+          }
+        },
+        "test-cov" :{
+          command: path.join("node_modules",".bin","istanbul") +" cover " + path.join("node_modules", "mocha", "bin", "_mocha") + " --dir " + path.join("frontend", "test_results", "coverage") + " -- -R json-cov > " +path.join("frontend", "test_results", "results.json"),
+              options: {
+              stdout: true
+   //           callback : builder.consolidateCoverageResults
+          }
+        },
+        ctest :{
+          command:"ctest -D Experimental",
+          options: {
+              stdout: true
+          }
+        }
+      },
+      clean: {
+        test: ["frontend/test_results/results.json"]
+      },
+      mkdir : {
+         test : {
+          options: {          
+            create: ['frontend/test_results']
+          }
+         }
+      },
+      env : {
+          mochaPlain : {
+            'mocha-unfunk-style' : "plain"
+          }
+      }
+  });
+
+  grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-mkdir');
+  grunt.loadNpmTasks('grunt-env');
+
+  grunt.registerTask('test', ['shell:test']);//Test
+  grunt.registerTask('istanbul', ["clean:test",'mkdir:test', 'shell:test-cov']);//Test Coverage results
+  grunt.registerTask('ctest', ['env:mochaPlain', 'shell:ctest']);//Pushes Test results to CDASH  
+
+};
--- frontend/app/calculator.js
+++ frontend/app/calculator.js
@@ -1,13 +1,15 @@
-exports.calculator = function(){
-  this.sub = function(a, b){
+exports.calculator = function() {
+  this.sub = function(a, b) {
     return a - b;
-  };
-  this.add = function(a, b){
+  }
+
+  this.add = function(a, b) {
     return a + b;
-  };
-  this.invalidAdd = function(a, b){
+  }
+
+  this.invalidAdd = function(a, b) {
     return a * (b || 0);
-  };
+  }
 
   return this;
-};
+}
