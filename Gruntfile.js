33122060e42e1ba8f7b0ba64356c4d34069885e4
krishnaindia <krishnaof1988@gmail.com>
2014-09-11 17:40:27 +0530

    Gruntfile.js validated to jscs

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,40 +1,40 @@
 var ci = require('./ci/ci');
 
-module.exports = function(grunt){
+module.exports = function(grunt) {
 
-  var ISTANBUL_COMMAND = "istanbul cover node_modules/mocha/bin/_mocha -- ";
+  var ISTANBUL_COMMAND = 'istanbul cover node_modules/mocha/bin/_mocha -- ';
 
   var CI_CONFIG = {
-      publishedFolder : "coverage",
-      scpBranchPath : {master : "temp", next : "temp_next"},
-      jsonReportFileName : "results.json",
-      jscsReportFileName : "jscs.txt"
-    }
-  
+    publishedFolder : 'coverage',
+    scpBranchPath : {master : 'temp', next : 'temp_next'},
+    jsonReportFileName : 'results.json',
+    jscsReportFileName : 'jscs.txt'
+  }
+
   grunt.initConfig({
     shell : {
       test : {
-        command: ISTANBUL_COMMAND + "-R mocha-unfunk-reporter"
+        command: ISTANBUL_COMMAND + '-R mocha-unfunk-reporter'
       },
       jscs : {
-        command:"jscs . -r text > " + CI_CONFIG.publishedFolder + "/" + CI_CONFIG.jscsReportFileName,
-        options : {         
+        command:'jscs . -r text > ' + CI_CONFIG.publishedFolder + '/' + CI_CONFIG.jscsReportFileName,
+        options : {
           callback : ci.codeStyleChecker
         }
       },
-      scp : {      
-        command: function(path){          
-          return "scp -r ./coverage/lcov-report/*  root@visualiser.maidsafe.net:/usr/maidsafe/" + path + "/frontend/test_results"
-        }        
+      scp : {
+        command: function(path) {
+          return 'scp -r ./coverage/lcov-report/*  root@visualiser.maidsafe.net:/usr/maidsafe/' + path + '/frontend/test_results'
+        }
       },
       ci : {
-        command : ISTANBUL_COMMAND + "-R json-cov > " + CI_CONFIG.publishedFolder + "/" + CI_CONFIG.jsonReportFileName,
+        command : ISTANBUL_COMMAND + '-R json-cov > ' + CI_CONFIG.publishedFolder + '/' + CI_CONFIG.jsonReportFileName,
         options : {
-          callback : ci.coverageCompleted 
+          callback : ci.coverageCompleted
         }
       },
       gitBranch : {
-        command : "git rev-parse --abbrev-ref HEAD",
+        command : 'git rev-parse --abbrev-ref HEAD',
         options : {
           callback : ci.gitBranchDetected
         }
@@ -45,7 +45,7 @@ module.exports = function(grunt){
     },
     mkdir : {
        test : {
-        options: {          
+        options: {
           create: [CI_CONFIG.publishedFolder]
         }
       }
@@ -55,9 +55,9 @@ module.exports = function(grunt){
   grunt.loadNpmTasks('grunt-shell');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-mkdir');
-  
+
   ci.init(grunt, CI_CONFIG);
 
-  grunt.registerTask('test', ["clean:test", 'mkdir:test', 'shell:jscs', 'shell:test']);//Test 
-  grunt.registerTask('ci', ["shell:gitBranch", "clean:test", 'mkdir:test', 'shell:jscs', 'shell:ci', 'shell:test']);//'shell:test-cov', 'shell:scp']);//Test Coverage results 
+  grunt.registerTask('test', ['clean:test', 'mkdir:test', 'shell:jscs', 'shell:test']);
+  grunt.registerTask('ci', ['shell:gitBranch', 'clean:test', 'mkdir:test', 'shell:jscs', 'shell:ci', 'shell:test']);
 };
